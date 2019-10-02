d35f175d92656eda92f6bad9feb526afe32a9f00
Mauricio Juárez Ibáñez <44931871+zJuarez@users.noreply.github.com>
2019-10-02 11:00:38 -0500

    Update helloWorld.cpp

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -2,11 +2,14 @@
 using namespace std;
 
 int main()
-{
-if(true){
-cout<<"Hello GitHub"<<endl;
-}
-else
-cout<<"Bye GitHub"<<endl;
-return 0;
-}
+    {
+      if(true)
+          {
+          cout<<"Hello GitHub"<<endl;
+          }
+            else
+            {
+            cout<<"Bye GitHub"<<endl;
+            }
+      return 0;
+      }
